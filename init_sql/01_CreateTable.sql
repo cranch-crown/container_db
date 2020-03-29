c7d8e6a32de0f3f67ebd3c1520bb208d8113c440
Y.Maeshima <ymzlgcranch@gmail.com>
2020-03-29 16:02:07 +0900

    データ投入サンプルを追加

---
+++ init_sql/01_CreateTable.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE test.departments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+)ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+CREATE TABLE test.employees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    last_name VARCHAR(10) NOT NULL,
+    first_name VARCHAR(10) NOT NULL,
+    department_id INT NOT NULL,
+    age INT NOT NULL,
+    CONSTRAINT fk_department_id
+        FOREIGN KEY (department_id) 
+        REFERENCES departments (id)
+        ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
---
+++ init_sql/02_InsertData.sql
@@ -0,0 +1,4 @@
+INSERT INTO test.departments (name) VALUES ('営業部');
+INSERT INTO test.departments (name) VALUES ('開発部');
+INSERT INTO test.departments (name) VALUES ('人事部');
+INSERT INTO test.departments (name) VALUES ('サービス部');
