4bf3566508e5600958d238b588d14aaf89e3737e
Y.Maeshima <ymzlgcranch@gmail.com>
2020-03-30 15:45:00 +0900

    データを追加

--- init_sql/02_InsertData.sql
+++ init_sql/02_InsertData.sql
@@ -2,9 +2,8 @@ INSERT INTO test.departments (name) VALUES ('営業部');
 INSERT INTO test.departments (name) VALUES ('開発部');
 INSERT INTO test.departments (name) VALUES ('人事部');
 INSERT INTO test.departments (name) VALUES ('サービス部');
-
-INSERT INTO test.employees (lase_name, first_name, department_id, age) VALUES ('小阪', '靖', 1, 31);
-INSERT INTO test.employees (lase_name, first_name, department_id, age) VALUES ('日向', '史郎', 3, 59);
-INSERT INTO test.employees (lase_name, first_name, department_id, age) VALUES ('倉野', '健', 2, 39);
-INSERT INTO test.employees (lase_name, first_name, department_id, age) VALUES ('緒川', '憂', 4, 24);
-INSERT INTO test.employees (lase_name, first_name, department_id, age) VALUES ('金岡', '美佳子', 4, 49);
+INSERT INTO test.employees (last_name, first_name, department_id, age) VALUES ('小阪', '靖', 1, 31);
+INSERT INTO test.employees (last_name, first_name, department_id, age) VALUES ('日向', '史郎', 3, 59);
+INSERT INTO test.employees (last_name, first_name, department_id, age) VALUES ('倉野', '健', 2, 39);
+INSERT INTO test.employees (last_name, first_name, department_id, age) VALUES ('緒川', '憂', 4, 24);
+INSERT INTO test.employees (last_name, first_name, department_id, age) VALUES ('金岡', '美佳子', 4, 49);
